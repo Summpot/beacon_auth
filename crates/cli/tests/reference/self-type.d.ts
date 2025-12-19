2b0e30ea6bf52e01cc66e967c244c3c44d1e359c
Summpot <summpot@outlook.com>
2025-12-20 00:27:32 +0800

    Squashed 'thirdparty/worker-rs/wasm-bindgen/' content from commit 11831fb8
    git-subtree-dir: thirdparty/worker-rs/wasm-bindgen
    git-subtree-split: 11831fb89d02ea80b2f72e10868e913d87156c06

---
+++ crates/cli/tests/reference/self-type.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export class Test {
+  free(): void;
+  [Symbol.dispose](): void;
+  consume_self(): void;
+  ref_mut_self(): void;
+  self_ref_Self(): void;
+  self_ref_mut_Self(): void;
+  constructor();
+  ref_self(): void;
+  self_Self(): void;
+}
