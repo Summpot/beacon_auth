08de0ae2f15089a8aa4b633d4fc9e17ccd74958b
Summpot <summpot@outlook.com>
2026-01-17 23:51:28 +0800

    feat: update Sentry middleware configuration to enable logs and set traces sample rate

--- src/start.ts
+++ src/start.ts
@@ -8,7 +8,9 @@ const sentryMiddleware = createMiddleware().server(({ next, request }) => {
       const { id: versionId } = env.CF_VERSION_METADATA;
       return {
         dsn: 'https://d7cb62a21d7b73d3c3db8dcd262a890b@o4510041882755072.ingest.us.sentry.io/4510726472728576',
+        tracesSampleRate: 1.0,
         release: versionId,
+        enableLogs: true,
         sendDefaultPii: true,
       };
     });
